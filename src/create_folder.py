5561c003ef945983968cec52b45c664ddf632efd
Abhishek <abhishekmarwade2109@gmail.com>
2025-03-06 21:45:08 +0530

    Day 1 - 7

--- src/create_folder.py
+++ src/create_folder.py
@@ -2,14 +2,14 @@ import os
 import shutil
 import argparse
 import yaml
-import pandas as pd 
+import pandas as pd
 import numpy as np
 from get_data import get_data, read_params
 
-################ CREATING FOLDER - STAR ################
+################# CREATING FOLDER - START ################
 def create_fold(config, image=None):
-    config = get_data(config)
-    dirr = config['load_data']['preprocessed_data']
+    config=get_data(config)
+    dirr = config['load_data']['preproseesd_data']
     cla = config['load_data']['num_classes']
     #print(dirr)
     #print(cla)
@@ -24,9 +24,9 @@ def create_fold(config, image=None):
             os.mkdir(dirr+'/'+'test'+'/'+'class_'+str(i))
         print('Folder Created Successfully....!')
 
-################ CREATING FOLDER - END ################
+################# CREATING FOLDER - END ################
 
-if __name__ == '__main__':
+if __name__=='__main__':
     args=argparse.ArgumentParser()
     args.add_argument('--config',default='params.yaml')
     passed_args=args.parse_args()
